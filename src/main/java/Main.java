92f289aa35a74daba0343f45bd3bd1c7cf38107e
Catalina Iuliana Luca <Catalina-Iuliana.Luca@endava.com>
2022-06-20 18:04:52 +0300

    added TimeStamps

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final long startTime=System.currentTimeMillis();
         int maximumNumberOfDivisors=0;
         ExecutorService executorService= Executors
                 .newFixedThreadPool(10);
@@ -18,7 +19,10 @@ public class Main {
             if(value>maximumNumberOfDivisors)maximumNumberOfDivisors=value;
         }
         System.out.println(maximumNumberOfDivisors);
+        final long timeFirstEnded=System.currentTimeMillis();
+        System.out.println("Time for multi threaded solution in milliseconds: "+(timeFirstEnded-startTime));
         sigleThreadSolution();
+        System.out.println("Time for single threaded solution in milliseconds: "+(System.currentTimeMillis()-timeFirstEnded));
     }
     public static void sigleThreadSolution(){
         int maximumNumberOfDivisors=0;
